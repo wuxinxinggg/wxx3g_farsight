964788928224a70f8d105644b91e7efc4bcf9687
wuxinxinggg <wuxinxinggg@gmail.com>
2012-07-27 14:53:20 +0800

    change the write_read_copy.c add a sequence stack program

--- file_io/write_read_copy.c
+++ file_io/write_read_copy.c
@@ -5,6 +5,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <errno.h>
+
 
 int main(int argc,char *argv[])
 {
@@ -13,6 +15,7 @@ int main(int argc,char *argv[])
 	int read_size;
 	int write_size;
 	char buf[BUFSIZ];
+	char *ptr;
 	int before_read_size_1,before_read_size_2,unwrite;
 	int before_read_size_all_2 = 0,before_read_size_all_1 = 0;
 	int offset;
@@ -25,14 +28,18 @@ int main(int argc,char *argv[])
 		exit(1);
 	}
 
-	write_fd = open(argv[2],O_RDONLY);//the file of argv[2] should exist,or program will go wrong
-	if(write_fd == -1)	
+	write_fd = open(argv[2],O_RDWR|O_CREAT|O_EXCL,0644);//if the file is not exist,creat it,or open it 
+	if(write_fd == -1)
 	{
-		perror("open");
-		exit(1);
+		write_fd = open(argv[2],O_RDONLY);//the file of argv[2] should exist,or program will go wrong
+		if(write_fd == -1)	
+		{
+			perror("open");
+			exit(1);
+		}
 	}
 
-	//////////////////////////////go on to copy the lines if something go wrong//////////////////////////////////
+	//////////////////////////////continue to copy the lines if something go wrong and exit//////////////////////////////////
 	
 	memset(buf,'\0',sizeof(buf));
 	before_read_size_1 = read(read_fd,buf,sizeof(buf));
@@ -59,7 +66,9 @@ int main(int argc,char *argv[])
 	printf("before_read_size_all_1 = %d\n",before_read_size_all_1);
 	printf("before_read_size_all_2 = %d\n",before_read_size_all_2);
 
-	if(before_read_size_all_1 > before_read_size_all_2)
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	if(before_read_size_all_1 > before_read_size_all_2) //if before_read_size_all_1 is lagrger \
+		than before_read_size_all_2,means the file is not equal to the read file ...write with append
 	{
 		close(write_fd);
 
@@ -84,31 +93,29 @@ int main(int argc,char *argv[])
 		printf("offset = %d\n",offset);
 
 		memset(buf,'\0',sizeof(buf));
-		read_size = read(read_fd,buf,sizeof(buf));
 
-		printf("read_size = %d\n",read_size);
-		while(read_size > 0)
+	///////////////////////////////////////////this is the normal read and write function/////////////////////////////////////////////////////////////////
+		while(read_size = read(read_fd,buf,sizeof(buf)))
 		{	
-			if(read_size < sizeof(buf))
+			if((read_size == -1) && (errno != EINTR)) //read error
+				break;
+			else if(read_size > 0)
 			{
-	
-				write_size = write(write_fd,buf,read_size); //the function of write return the bytes that write successfully
-				printf("write_size = %d\n",write_size);
-			}
-
-			else
-			{
-				while(read_size > 0)
+				ptr = buf; //to remember the last written position 
+				while(write_size = write(write_fd,ptr,read_size)) //the function of write return the bytes that write successfully
 				{
-					write_size = write(write_fd,buf,read_size);
-					printf("else: write_size = %d\n",write_size);
-
-					memset(buf,'\0',sizeof(buf));
-					read_size = read(read_fd,buf,sizeof(buf));
+					if((write_size == -1)) //write error
+						break;
+					else if(write_size == read_size) //write finished
+						break;
+					else if(write_size > 0)
+					{
+						read_size -=write_size;
+						ptr +=write_size;
+					}
 				}
 			}
 		}
-
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////
